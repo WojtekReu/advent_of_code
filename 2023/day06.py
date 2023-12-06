ddc6dcf2505b73c1d0d8d816121a0c4c59d0996b
Wojciech Zając <wojtek@reu.pl>
2023-12-06 10:32:17 +0100

    Training for year 2023 - day 6; calculated by the formula

--- 2023/day06.py
+++ 2023/day06.py
@@ -3,28 +3,52 @@
 https://adventofcode.com/2023/day/06
 """
 FILENAME = "day06.input.txt"
-from math import prod
+from math import prod, sqrt
 
 
-def read_input(filename):
+def read_input(filename: str) -> str:
     with open(filename, "r") as f:
         return f.read()
 
 
-def prepare(data):
+def prepare(data: str) -> list[tuple]:
     race_time = [int(v) for v in data.split("\n")[0].split(":")[1].split()]
     race_dist = [int(v) for v in data.split("\n")[1].split(":")[1].split()]
     races = [r for r in zip(race_time, race_dist)]
     return races
 
 
-def one_race(races):
+def input_for_one_race(races: list[tuple]) -> tuple[int, int]:
     time = int("".join(str(x[0]) for x in races))
     dist = int("".join(str(x[1]) for x in races))
     return time, dist
 
 
-def calculate(races):
+def fast_calculation(races: list[tuple]) -> int:
+    """
+    The rounding beaten_records some times is different by 1 from real beaten records value.
+    """
+    records_product = 1
+    for rtime, rdist in races:
+        beaten_records = count_beaten_records(rtime, rdist)
+        records_product *= int(beaten_records)
+    return records_product
+
+
+def count_beaten_records(time: int, record: int) -> float:
+    """
+    Solve the inequality: record < x * time - x**2
+    """
+    # x1 = int((time - sqrt(pow(time, 2) - 4 * record)) / 2)
+    # x2 = int((time + sqrt(pow(time, 2) - 4 * record)) / 2)
+    # return x2 - x1
+    return sqrt(pow(time, 2) - 4 * record)
+
+
+def calculate(races: list[tuple]) -> int:
+    """
+    Very slow calculation
+    """
     records = []
     for rtime, rdist in races:
         record_count = 0
@@ -33,12 +57,14 @@ def calculate(races):
             total_dist = time_left * load
             if rdist < total_dist:
                 record_count += 1
-                # print(f"{rtime = } \t {load = } {total_dist = }")
         records.append(record_count)
     return prod(records)
 
 
-def calculate2(rtime, rdist):
+def calculate2(rtime: int, rdist: int) -> int:
+    """
+    Slow calculation
+    """
     not_record = 0
     for load in range(rtime):
         time_left = rtime - load
@@ -65,6 +91,6 @@ if __name__ == "__main__":
     r = prepare(data)
     result = calculate(r)
     print(f"Multiply the numbers of ways you could beat the record is: {result}.")
-    t, d = one_race(r)
-    result2 = calculate2(t, d)
-    print(f"You can beat record on {result2} ways.")
+    t, d = input_for_one_race(r)
+    result2 = count_beaten_records(t, d)
+    print(f"For time={t} you can beat distance record={d} on {int(result2)} ways.")
