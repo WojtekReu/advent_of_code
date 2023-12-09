94d7a367070ae7e186ebf332b2617fb2496b09f4
Wojciech Zając <wojtek@reu.pl>
2023-12-09 08:11:20 +0100

    Training for year 2023 - day 9; P2

--- 2023/day09.py
+++ 2023/day09.py
@@ -16,33 +16,33 @@ def prepare(data: str):
     return total_history
 
 
-def get_next_value(history):
-    v0 = None
-    history_new = []
-    for v in history:
-        if v0 is not None:
-            history_new.append(v - v0)
-        v0 = v
+def get_next_value(history, go_back):
+    history_new = [history[i + 1] - history[i] for i in range(len(history) - 1)]
     if any(history_new):
-        v_next = get_next_value(history_new)
-        return history[-1] + v_next
+        v_next = get_next_value(history_new, go_back)
+        if go_back:
+            return history[0] - v_next
+        else:
+            return history[-1] + v_next
     else:
         return history[-1] + history_new[-1]
 
-def calculate(total_history):
+
+def calculate(total_history, go_back=False):
     v_sum = 0
     for history in total_history:
-        # print(history)
-        v = get_next_value(history)
-        # print(v)
+        v = get_next_value(history, go_back)
         v_sum += v
     return v_sum
-
 
 
 if __name__ == "__main__":
     data = read_input(FILENAME)
     th = prepare(data)
     result = calculate(th)
-    print(f"res 1 :{result}")
-    # assert result == 11199999
+    print(f"The sum of extrapolated values in P1 is {result}.")
+    assert result == 2075724761
+
+    result = calculate(th, True)
+    print(f"The sum of extrapolated values in P2 is {result}.")
+    assert result == 1072
