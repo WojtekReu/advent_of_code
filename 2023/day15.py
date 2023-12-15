c88d1eca4a81e8321b9a7d41f9c251dc00be718c
Wojciech Zając <wojtek@reu.pl>
2023-12-15 09:47:01 +0100

    Training for year 2023 - day 15; P2

--- 2023/day15.py
+++ 2023/day15.py
@@ -16,21 +16,66 @@ def prepare(data: str):
     return seq
 
 
-def calculate(seq):
+def get_label(k: str) -> str:
+    if "-" in k:
+        return k.replace("-", "")
+    return k.split("=")[0]
+
+
+def get_focusing_power(k: str) -> int:
+    return int(k.split("=")[1])
+
+
+def get_hash(label: str) -> int:
+    current_value = 0
+    for c in label:
+        current_value += ord(c)
+        current_value *= 17
+        current_value %= 256
+    return current_value
+
+
+def calculate(seq: list) -> tuple[int, list]:
     seq_sum = 0
+    lenses = []
     for k in seq:
-        current_value = 0
-        for c in k:
-            current_value += ord(c)
-            current_value *= 17
-            current_value %= 256
-        seq_sum += current_value
-    return seq_sum
+        seq_sum += get_hash(k)
+        lenses.append((k, get_hash(get_label(k))))
+    return seq_sum, lenses
+
+
+def calculate2(lenses: list) -> int:
+    boxes = [[] for _ in range(256)]
+    for lens, box_id in lenses:
+        box = boxes[box_id]
+        label = get_label(lens)
+        if "-" in lens:
+            box[:] = [l for l in box if l[0] != label]
+        elif "=" in lens:
+            exists = False
+            for l in box:
+                if l[0] == label:
+                    l[1] = get_focusing_power(lens)
+                    exists = True
+
+            if not exists:
+                box.append([label, get_focusing_power(lens)])
+
+    total_focusing_power = 0
+    for i, box in enumerate(boxes, start=1):
+        for j, slot in enumerate(box, start=1):
+            total_focusing_power += i * j * slot[1]
+
+    return total_focusing_power
 
 
 if __name__ == "__main__":
     data = read_input(FILENAME_INPUT)
     d = prepare(data)
-    res1 = calculate(d)
+    res1, l = calculate(d)
     print(f"The sum of the result is {res1}.")
     assert res1 == 504036
+
+    res2 = calculate2(l)
+    print(f"For the resulting lens configuration focusing power is {res2}.")
+    assert res2 == 295719
