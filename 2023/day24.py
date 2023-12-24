559800b32fa7bbeb4cee8a432763c357c94a6574
Wojciech Zając <wojtek@reu.pl>
2023-12-24 21:42:55 +0100

    Training for year 2023 - day 24; P2

--- 2023/day24.py
+++ 2023/day24.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
 https://adventofcode.com/2023/day/24
-pypy3 real time 0m0,232s
+pypy3 real time 0m4,438s
 """
 from collections import namedtuple
 from itertools import combinations
+from sympy import Symbol, solve
 
 FILENAME_TEST = "day24.test.txt"
 FILENAME_INPUT = "day24.input.txt"
@@ -33,13 +34,13 @@ def calculate(hailstones, check_area):
     for h1, h2 in combinations(hailstones, 2):
         a1 = h1.vy / h1.vx
         a2 = h2.vy / h2.vx
-        b1 = -a1 * h1.x + h1.y
-        b2 = -a2 * h2.x + h2.y
+        b1 = h1.y - a1 * h1.x
+        b2 = h2.y - a2 * h2.x
 
         try:
             p_x = (b2 - b1) / (a1 - a2)
         except ZeroDivisionError:
-            continue
+            continue  # hails don't collide, theirs paths are parallel.
         p_y = a1 * p_x + b1
 
         if check_area[0] <= p_x <= check_area[1] and check_area[0] <= p_y <= check_area[1]:
@@ -51,10 +52,47 @@ def calculate(hailstones, check_area):
     return len(collides)
 
 
+def calculate2(hailstones):
+    h1 = hailstones[0]
+    h2 = hailstones[1]
+    h3 = hailstones[2]
+
+    cr_x = Symbol("cr_x")
+    cr_y = Symbol("cr_y")
+    cr_z = Symbol("cr_z")
+    cr_vx = Symbol("cr_vx")
+    cr_vy = Symbol("cr_vy")
+    cr_vz = Symbol("cr_vz")
+    t1 = Symbol("t1")
+    t2 = Symbol("t2")
+    t3 = Symbol("t3")
+
+    equation = [
+        cr_x + cr_vx * t1 - h1.x - h1.vx * t1,
+        cr_y + cr_vy * t1 - h1.y - h1.vy * t1,
+        cr_z + cr_vz * t1 - h1.z - h1.vz * t1,
+
+        cr_x + cr_vx * t2 - h2.x - h2.vx * t2,
+        cr_y + cr_vy * t2 - h2.y - h2.vy * t2,
+        cr_z + cr_vz * t2 - h2.z - h2.vz * t2,
+
+        cr_x + cr_vx * t3 - h3.x - h3.vx * t3,
+        cr_y + cr_vy * t3 - h3.y - h3.vy * t3,
+        cr_z + cr_vz * t3 - h3.z - h3.vz * t3,
+    ]
+    solution = solve(equation, cr_x, cr_y, cr_z, cr_vx, cr_vy, cr_vz, t1, t2, t3)
+    print(solution)
+    return sum(solution[0][:3])
+
+
 if __name__ == "__main__":
     data = read_input(FILENAME_INPUT)
     h = prepare(data)
+
     res1 = calculate(h, CHECK_AREA)
     print(f"The number of intersections that occur within the test area is {res1}.")
-
     assert res1 == 20963
+
+    res2 = calculate2(h)
+    print(f"When you add up X, Y, and Z coordinates of the initial position you get {res2}.")
+    assert res2 == 999782576459892
