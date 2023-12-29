ad1571d165e742c99067f05864dd5c17ee05cf24
Wojciech Zając <wojtek@reu.pl>
2023-12-29 13:19:53 +0100

    AoC 2023 day21; P2 enhancement

--- 2023/day21.py
+++ 2023/day21.py
@@ -12,20 +12,20 @@ MAX_STEPS_TEST = 6
 MAX_STEPS_INPUT_P1 = 64
 MAX_STEPS_INPUT_P2 = 26501365
 
+# Platform namedtuple has adjacent Point and coords changes correlated to current Point. That means dy and dx can be 0, 1 or -1
 Platform = namedtuple("Platform", ["dy", "dx", "point"])
 
 
 class Point:
-    grid: list
-    visited = defaultdict(int)
+    visited_on_platform = defaultdict(int)  # it's global counter for all points
 
     def __init__(self, y, x, c):
         self.y = y
         self.x = x
         self.c = c
         self.adjacent: list[Platform] = []
-        self.steps = set()
-        self.v1 = set()
+        # visited is only for show function to display grid with visited points
+        self.visited = set()  # ("0,0"), ("-1,0"), ("3,-2"), ... it's for the one point
 
     def __repr__(self):
         return f"<{self.y},{self.x} {self.c}>"
@@ -99,15 +99,15 @@ class Gardener:
                 self.step + 1,
             )
 
-    def is_even(self, shift) -> bool:
-        if (self.step + shift) % 2:  # shift is 0 for odd or 1 for even max_step_number
+    def is_even(self) -> bool:
+        if not self.step % 2:  # shift is 0 for odd or 1 for even max_step_number
             self.check_on_grid()
             return True
         return False
 
     def check_on_grid(self):
-        self.point.visited[f"{self.platform_y},{self.platform_x}"] += 1
-        self.point.v1.add(f"{self.platform_y},{self.platform_x}")
+        self.point.visited_on_platform[f"{self.platform_y},{self.platform_x}"] += 1
+        self.point.visited.add(f"{self.platform_y},{self.platform_x}")
 
 
 def read_input(filename: str) -> str:
@@ -117,7 +117,6 @@ def read_input(filename: str) -> str:
 
 def prepare(data: str):
     grid = [[Point(y, x, c) for x, c in enumerate(line)] for y, line in enumerate(data.split("\n"))]
-    Point.grid = grid
 
     y_max = len(grid) - 1
     x_max = len(grid[0]) - 1
@@ -138,65 +137,72 @@ def prepare(data: str):
 
             if p.c == "S":
                 point_start: Point = p
-                point_start.steps.add("0,0")
-                point_start.visited["0,0"] = 1
+                point_start.visited_on_platform["0,0"] = 1
             if p.c != "#":
                 available_coords.add(f"{p.y},{p.x}")
 
     return point_start, available_coords, grid
 
 
-def show(grid, max_steps):
-    n = 2
-    xy_len = (n + 1) * 131
-    xy_0 = -n * 131
+def show(grid, max_step):
+    xy_max = max_step + 66
+    xy_min = 65 - max_step
 
-    count_star = 1
+    count_star = 0
     output = ""
-    for y in range(xy_0, xy_len):
+    for y in range(xy_min, xy_max):
         line = ""
-        for x in range(xy_0, xy_len):
+        for x in range(xy_min, xy_max):
             platform_y, p_y = divmod(y, 131)
             platform_x, p_x = divmod(x, 131)
-            p = grid[p_y][p_x]
-            platform = f"{platform_y},{platform_x}"
-            if platform in p.v1:
+            if f"{platform_y},{platform_x}" in grid[p_y][p_x].visited:
                 count_star += 1
                 line += "*"
             else:
-                line += p.c
+                line += grid[p_y][p_x].c
         output = f"{output}{line}\n"
 
-    print(output)
-    print(f"for {max_steps}: {count_star = }")
+    print(output, end="")
+    print(f"For {max_step} steps program counted {count_star} stars.\n")
 
 
 def calculate(point_start, available_coords, max_steps):
-    shift = 0 if max_steps % 2 else 1
+    Point.visited_on_platform = defaultdict(int)
     Gardener.available_coords_org = available_coords
     Gardener.available_platforms = {"0,0": available_coords.copy()}
-    Gardener.available_platforms["0,0"].remove(f"{point_start.y},{point_start.x}")
 
-    points_count = shift  # for even starts from 1 but for odd starts from 0
+    points_count = 0
     g_queue = deque()
-    g_queue.append(Gardener(0, 0, point_start, 0))
+    if max_steps % 2:
+        for platform in point_start.adjacent:
+            g_queue.append(Gardener(0, 0, platform.point, 0))
+    else:
+        g_queue.append(Gardener(0, 0, point_start, 0))
 
     while g_queue:
         gardener = g_queue.popleft()
         for gardener_next in gardener.go(max_steps):
             g_queue.append(gardener_next)
-            if gardener_next.is_even(shift):
+            if gardener_next.is_even():
                 points_count += 1
 
     return points_count
 
 
-def calculate_for_grid(point_start, n):  # n = 202300
+def calculate_for_grid(point_start, max_step):
+    n, rest = divmod((max_step - 65), 131)  # for P2 n = 202300
+    if rest:
+        raise ValueError(
+            f"For max_step - 65 the rest has to be 0 but is: ({max_step} - 65) % 131 = {rest}"
+        )
+    pattern_n = 3 if n % 2 else 4
+    pattern_value = pattern_n * 131 + 65  # for n=3 -> max_step=458; for n=4 -> max_step=589
+    calculate(ps, ac, pattern_value)
     k = sum([i * 8 for i in range(n // 2)])
     l = pow(n, 2)
     m = n - 1
     p = pow(n - 1, 2)
-    v = point_start.visited
+    v = point_start.visited_on_platform
     # for a, b in v.items():
     #     print(a, b)
     # for a, b in Counter([x for x in v.values()]).items():
@@ -224,7 +230,6 @@ def calculate_for_grid(point_start, n):  # n = 202300
             + v["-4,0"]
             + v["4,0"]
             + n * (v["-2,-3"] + v["2,-3"] + v["-2,3"] + v["2,3"])
-            - 1
         )
     return total
 
@@ -233,17 +238,10 @@ if __name__ == "__main__":
     data = read_input(FILENAME_INPUT)
     ps, ac, g = prepare(data)
     res1 = calculate(ps, ac, MAX_STEPS_INPUT_P1)
+    # show(g, MAX_STEPS_INPUT_P1)
     print(f"In exactly {MAX_STEPS_INPUT_P1} steps the Elf could reach {res1} garden plots.")
     assert res1 == 3740
 
-    ps, ac, g = prepare(data)
-    n = (MAX_STEPS_INPUT_P2 - 65) // 131
-    if n % 2:
-        even_steps = 3 * 131 + 65
-    else:
-        even_steps = 4 * 131 + 65
-    calculate(ps, ac, even_steps)
-
-    res2 = calculate_for_grid(ps, n)
-    print(f"In exactly {MAX_STEPS_INPUT_P2} steps the Elf could reach {res2 = } garden plots.")
+    res2 = calculate_for_grid(ps, MAX_STEPS_INPUT_P2)
+    print(f"In exactly {MAX_STEPS_INPUT_P2} steps the Elf could reach {res2} garden plots.")
     assert res2 == 620962518745459
