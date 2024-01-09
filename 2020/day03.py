dd8096a67393b8cba8cb3475fa1f59058289e8f2
Wojciech Zając <wojtek@reu.pl>
2024-01-09 20:48:11 +0100

    AoC 2020 day03; P1, P2.

---
+++ 2020/day03.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+https://adventofcode.com/2020/day/03
+"""
+
+FILENAME_INPUT = "day03.input.txt"
+SLOPES = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+
+
+def read_input(filename: str) -> str:
+    with open(filename, "r") as f:
+        return f.read()
+
+
+def prepare(data: str):
+    tree_map = [line for line in data.split("\n")]
+    return tree_map
+
+
+def calculate(tree_map: list[str], right: int = 3, down: int = 1) -> int:
+    trees = 0
+    step = 0
+    for i, line in enumerate(tree_map):
+        if i % down:
+            continue
+        try:
+            square = line[step]
+        except IndexError:
+            step = step - len(line)
+            square = line[step]
+        if square == "#":
+            trees += 1
+        step += right
+    return trees
+
+
+def calculate2(tree_map: list[str]):
+    multiply = 1
+    for right, down in SLOPES:
+        trees = calculate(tree_map, right, down)
+        multiply *= trees
+    return multiply
+
+
+if __name__ == "__main__":
+    data = read_input(FILENAME_INPUT)
+    tm = prepare(data)
+    result1 = calculate(tm)
+    print(f"You would encounter {result1} trees.")
+
+    result2 = calculate2(tm)
+    print(
+        f"You get {result2} if you multiply together encountered trees on each of the listed slopes."
+    )
+
+    assert result1 == 193
+    assert result2 == 1355323200
